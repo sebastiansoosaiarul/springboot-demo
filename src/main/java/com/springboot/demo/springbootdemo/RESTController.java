2ee482fe3eaa779c925290fbb1677aa3937b53f2
Sebastian <sebastian.soosaiarul@gmail.com>
2018-02-01 19:16:58 -0500

    initial versions

---
+++ src/main/java/com/springboot/demo/springbootdemo/RESTController.java
@@ -0,0 +1,15 @@
+package com.springboot.demo.springbootdemo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class RESTController {
+	
+	    @RequestMapping("/home")
+	    public String home() {
+	        return "<html><h1>Spring boot application home!</h1></html>";
+	    }
+}
