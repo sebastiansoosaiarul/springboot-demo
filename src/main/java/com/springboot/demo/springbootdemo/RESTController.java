0ffef785dd986f9494e77faa76b5f38cb554eed1
sebastiansoosaiarul <sebastian.soosaiarul@gmail.com>
2018-02-02 13:18:38 -0500

    Update RESTController.java

--- src/main/java/com/springboot/demo/springbootdemo/RESTController.java
+++ src/main/java/com/springboot/demo/springbootdemo/RESTController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@RequestMapping("/springboot-demo")
 public class RESTController {
 	
 	    @RequestMapping("/home")
